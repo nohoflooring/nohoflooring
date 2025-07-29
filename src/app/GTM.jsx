319b0f7bbd172244546b89500852641569e52bbe
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-07-29 21:12:41 +0500

    tag added

--- src/app/GTM.jsx
+++ src/app/GTM.jsx
@@ -1,45 +1,56 @@
 "use client";
 import Script from "next/script";
 
-const GTM_ID = "GTM-NRFFWDZL";
-const GA_ID = "G-3ZPMNEJ9DJ";
-
 export default function GTM() {
     return (
         <>
             {/* Google Tag Manager */}
             <Script id="gtm-script" strategy="afterInteractive">
                 {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-NRFFWDZL');
+                `}
             </Script>
 
             {/* Google Analytics */}
             <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                src="https://www.googletagmanager.com/gtag/js?id=G-3ZPMNEJ9DJ"
                 strategy="afterInteractive"
             />
             <Script id="ga-script" strategy="afterInteractive">
                 {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_ID}');
-        `}
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-3ZPMNEJ9DJ');
+                `}
             </Script>
 
-            {/* Google Tag Manager (noscript) */}
+            {/* Google Ads Conversion Tracking */}
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=AW-966030310"
+                strategy="afterInteractive"
+            />
+            <Script id="ads-script" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'AW-966030310');
+                `}
+            </Script>
+
+            {/* Google Tag Manager (noscript fallback) */}
             <noscript>
                 <iframe
-                    src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-NRFFWDZL"
                     height="0"
                     width="0"
                     style={{ display: "none", visibility: "hidden" }}
-                ></iframe>
+                />
             </noscript>
         </>
     );
