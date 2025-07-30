cbd1a74465cf96baa701d828e04cea340b7bcab3
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-07-30 19:05:21 +0500

    convertion tag install

--- src/app/thank-you/page.jsx
+++ src/app/thank-you/page.jsx
@@ -4,19 +4,27 @@ import { Col, Container, Row } from "react-bootstrap";
 
 const ThankYouPage = () => {
   return (
-    <section className={styles.thanksSection}>
-      <Container className="h-100">
-        <Row className="h-100">
-          <Col md={6} className="m-auto text-center">
-            <div className={styles.boxTitle}>
-              <h1>Thank You!</h1>
-              <p>We’ve received your request! Our team is reviewing your details and will be in touch soon with a customized flooring solution tailored to your needs.</p>
-              <Link href="/">Back To Home</Link>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+    <>
+      {/* Google Ads Conversion Script */}
+      <Script id="google-ads-conversion" strategy="afterInteractive">
+        {`
+          gtag('event', 'conversion', {'send_to': 'AW-966030310/432jCL35tPsaEObn0cwD'});
+        `}
+      </Script>
+      <section className={styles.thanksSection}>
+        <Container className="h-100">
+          <Row className="h-100">
+            <Col md={6} className="m-auto text-center">
+              <div className={styles.boxTitle}>
+                <h1>Thank You!</h1>
+                <p>We’ve received your request! Our team is reviewing your details and will be in touch soon with a customized flooring solution tailored to your needs.</p>
+                <Link href="/">Back To Home</Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   );
 };
 
