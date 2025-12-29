425e8305d1c5d51bf977af5ad3da4bfbd88b93ba
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-12-29 18:24:12 +0500

    next updare

--- next.config.js
+++ next.config.js
@@ -1,21 +1,26 @@
 const path = require("path");
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+
+  turbopack: {},
+
   webpack(config, { isServer }) {
     config.module.rules.push({
       test: /\.webm$/,
       use: {
         loader: "file-loader",
         options: {
-          publicPath: `/_next/static/videos/`,
+          publicPath: "/_next/static/videos/",
           outputPath: `${isServer ? "../" : ""}static/videos/`,
           name: "[name].[hash].[ext]",
         },
       },
     });
+
     return config;
   },
 };
