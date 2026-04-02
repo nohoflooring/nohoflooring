7be3abd1fc37a4762b5b3be8d6eb0d5d15da7481
Aziz Ur Rehman <dev.az@inhousedept.com>
2026-04-02 18:39:56 +0500

    Geo tag

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,10 +11,154 @@ import Blogs from "@/src/app/home/blogs"
 import Contact from "@/src/app/home/contact"
 import { BlogListingData } from "@/src/app/blogs/data/data"
 import { mainData, AbtData, ctaData, clientData, faqsData } from "@/src/app/home/data/data"
+import Head from 'next/head';
+
 
 const Page = () => {
   return (
     <>
+      <Head>
+        <meta name="DC.title" content="flooring contractors North Hollywood, flooring experts North Hollywood, hardwood flooring" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.165115;-118.365171" />
+        <meta name="ICBM" content="34.165115, -118.365171" />
+        <meta name="geo.postalcode" content="91601" />
+
+        <meta name="DC.title" content="flooring contractors North Hollywood, flooring experts North Hollywood, hardwood flooring" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.152169;-118.357141" />
+        <meta name="ICBM" content="34.152169, -118.357141" />
+        <meta name="geo.postalcode" content="91602" />
+
+        <meta name="DC.title" content="flooring contractors North Hollywood, flooring experts North Hollywood, hardwood flooring" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.16495;-118.374752" />
+        <meta name="ICBM" content="34.16495, -118.374752" />
+        <meta name="geo.postalcode" content="91605" />
+
+        <meta name="DC.title" content="flooring contractors North Hollywood, flooring experts North Hollywood, hardwood flooring" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.16495;-118.374752" />
+        <meta name="ICBM" content="34.16495, -118.374752" />
+        <meta name="geo.postalcode" content="91606" />
+
+        <meta name="DC.title" content="flooring contractors North Hollywood, flooring experts North Hollywood, hardwood flooring" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.163715;-118.396576" />
+        <meta name="ICBM" content="34.163715, -118.396576" />
+        <meta name="geo.postalcode" content="91607" />
+
+        <meta name="DC.title" content="flooring contractors North Hollywood, flooring experts North Hollywood, hardwood flooring" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.214885;-118.49982" />
+        <meta name="ICBM" content="34.214885, -118.49982" />
+        <meta name="geo.postalcode" content="91611" />
+
+        <meta name="DC.title" content="flooring contractors North Hollywood, flooring experts North Hollywood, hardwood flooring" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.073871;-118.233219" />
+        <meta name="ICBM" content="34.073871, -118.233219" />
+        <meta name="geo.postalcode" content="91612" />
+
+        <meta name="DC.title" content="flooring contractors North Hollywood, flooring experts North Hollywood, hardwood flooring" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.186247;-118.396581" />
+        <meta name="ICBM" content="34.186247, -118.396581" />
+        <meta name="geo.postalcode" content="91615" />
+
+        <meta name="DC.title" content="flooring contractors North Hollywood, flooring experts North Hollywood, hardwood flooring" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.position" content="34.315507;-118.209681" />
+        <meta name="ICBM" content="34.315507, -118.209681" />
+        <meta name="geo.postalcode" content="91616" />
+
+        <meta name="DC.title" content="flooring contractors North Hollywood, flooring experts North Hollywood, hardwood flooring" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.16495;-118.374752" />
+        <meta name="ICBM" content="34.16495, -118.374752" />
+        <meta name="geo.postalcode" content="91618" />
+
+        <meta name="DC.title" content="flooring contractors Studio City, flooring experts Studio City, hardwood flooring Studio City" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.148399;-118.396188" />
+        <meta name="ICBM" content="34.148399, -118.396188" />
+        <meta name="geo.postalcode" content="91604" />
+
+        <meta name="DC.title" content="flooring contractors Valley Glen, flooring experts Valley Glen, hardwood flooring Valley Glen" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.182638;-118.413867" />
+        <meta name="ICBM" content="34.182638, -118.413867" />
+        <meta name="geo.postalcode" content="91606" />
+
+        <meta name="DC.title" content="flooring contractors Valley Village, flooring experts Valley Village, hardwood flooring Valley Village" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.163715;-118.396576" />
+        <meta name="ICBM" content="34.163715, -118.396576" />
+        <meta name="geo.postalcode" content="91607" />
+        <meta name="geo.postalcode" content="91617" />
+
+        <meta name="DC.title" content="flooring contractors Universal City, flooring experts Universal City, hardwood flooring Universal City, flooring services in universal city" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Universal City, CA" />
+        <meta name="geo.position" content="34.141923;-118.358411" />
+        <meta name="ICBM" content="34.141923, -118.358411" />
+        <meta name="geo.postalcode" content="91608" />
+        <meta name="geo.postalcode" content="91618" />
+
+        <meta name="DC.title" content="flooring contractors Van Nuys, flooring experts Van Nuys, hardwood flooring Van Nuys" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.186619;-118.448667" />
+        <meta name="ICBM" content="34.186619, -118.448667" />
+        <meta name="geo.postalcode" content="91401" />
+        <meta name="geo.postalcode" content="91411" />
+        <meta name="geo.postalcode" content="91406" />
+
+        <meta name="DC.title" content="flooring contractors Burbank, flooring experts Burbank, hardwood flooring Burbank, flooring contractors near burbank, flooring contractors near me" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Burbank" />
+        <meta name="geo.position" content="34.181209;-118.307201" />
+        <meta name="ICBM" content="34.181209, -118.307201" />
+        <meta name="geo.postalcode" content="91522" />
+        <meta name="geo.postalcode" content="91521" />
+        <meta name="geo.postalcode" content="91523" />
+        <meta name="geo.postalcode" content="91506" />
+
+        <meta name="DC.title" content="flooring contractors Los Angeles, flooring experts Los Angeles, hardwood flooring Los Angeles" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="Los Angeles" />
+        <meta name="geo.position" content="34.053691;-118.242766" />
+        <meta name="ICBM" content="34.053691, -118.242766" />
+        <meta name="geo.postalcode" content="90034" />
+        <meta name="geo.postalcode" content="90019" />
+        <meta name="geo.postalcode" content="90004" />
+        <meta name="geo.postalcode" content="90027" />
+        <meta name="geo.postalcode" content="90029" />
+        <meta name="geo.postalcode" content="90046" />
+        <meta name="geo.postalcode" content="90046" />
+        <meta name="geo.postalcode" content="90066" />
+
+        <meta name="DC.title" content="flooring contractors West Hollywood, flooring experts West Hollywood, hardwood flooring West Hollywood, flooring service west hollywood ca, flooring service west hollywood, flooring services west hollywood" />
+        <meta name="geo.region" content="US-CA" />
+        <meta name="geo.placename" content="West Hollywood" />
+        <meta name="geo.position" content="34.092301;-118.369289" />
+        <meta name="ICBM" content="34.092301, -118.369289" />
+        <meta name="geo.postalcode" content="91001" />
+
+
+      </Head>
       <Banner />
       <About data={AbtData} />
       <Services home={true} />
