3980429dc323f070a5ee6d03e06ad821cb49d7a4
Ahsan Sahal <lauren@infinitidigital.us>
2026-07-01 19:54:16 -0400

    Script Update on Blog Page

---
+++ src/app/components/soro-blog-widget.jsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useEffect } from 'react';
+
+const SoroBlogWidget = () => {
+  useEffect(() => {
+    const container = document.getElementById('soro-blog');
+    if (!container) return;
+
+    const existingScript = document.querySelector('script[src*="trysoro.com/api/embed"]');
+    if (existingScript) return;
+
+    const script = document.createElement('script');
+    script.src = 'https://app.trysoro.com/api/embed/c6620b58-a5c-4b91-a491-d2568d62210c';
+    script.defer = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return <div id="soro-blog" style={{ margin: '2rem 0' }} />;
+};
+
+export default SoroBlogWidget;
