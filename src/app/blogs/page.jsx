4928293f6b257c1dbefc0bb90daf997efa334e53
Ahsan Sahal <lauren@infinitidigital.us>
2026-07-01 21:02:33 -0400

    Script Update on Blogpage

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -2,13 +2,11 @@ import Banner from '@/src/app/components/banner';
 import { BannerData, BlogListingData, mainData } from '@/src/app/blogs/data/data';
 import BlogListing from '@/src/app/components/blog/bloglisting';
 import Contact from '@/src/app/home/contact';
-import SoroBlogWidget from '@/src/app/components/soro-blog-widget';
 
 const Page = () => {
   return (
     <>
       <Banner data={BannerData} />
-      <SoroBlogWidget />
       <BlogListing data={mainData} dataListing={BlogListingData} />
       <Contact />
     </>
